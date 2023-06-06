1ac1cef1cc53ce63f7c67e7dd2b9736b5114f826
MitchellGerdisch <mitch@pulumi.com>
2023-06-05 21:03:20 -0500

    clean template

--- __main__.py
+++ __main__.py
@@ -1,17 +1,6 @@
-"""An AWS Python Pulumi program"""
+# App project Template
 
 import pulumi
 from pulumi_aws import s3
 
-# Main bucket
-platform_bucket = s3.BucketV2('platform-bucket')
-platform_bucket_public_access_block = s3.BucketPublicAccessBlock('platform-bucket-access', s3.BucketPublicAccessBlockArgs(
-    bucket=platform_bucket.id,
-    block_public_acls=True,
-    block_public_policy=True,
-    ignore_public_acls=True,
-    restrict_public_buckets=True,
-))
 
-# Export the name of the bucket
-pulumi.export('platform_bucket_name', platform_bucket.id)

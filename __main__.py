a4306765c9b47a5365cc3349e5f70e1b59a78556
MitchellGerdisch <mitch@pulumi.com>
2023-06-05 21:51:22 -0500

    Add block-public-policy

--- __main__.py
+++ __main__.py
@@ -8,5 +8,6 @@ app_bucket = s3.BucketV2("app-bucket")
 app_bucket_access_block = s3.BucketPublicAccessBlock("app-bucket-access-block", s3.BucketPublicAccessBlockArgs(
     bucket=app_bucket.id,
     block_public_acls=True,
+    block_public_policy=True,
 ))
 

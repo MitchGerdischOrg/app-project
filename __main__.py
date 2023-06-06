c9da47d065b50b51573d3756a055882a867fe274
MitchellGerdisch <mitch@pulumi.com>
2023-06-05 21:58:15 -0500

    remove block policy thing

--- __main__.py
+++ __main__.py
@@ -8,6 +8,5 @@ app_bucket = s3.BucketV2("app-bucket")
 app_bucket_access_block = s3.BucketPublicAccessBlock("app-bucket-access-block", s3.BucketPublicAccessBlockArgs(
     bucket=app_bucket.id,
     block_public_acls=True,
-    block_public_policy=True,
 ))
 

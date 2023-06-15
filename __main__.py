221ed5b5833d6667e7a3bffd01c5c9660cac0f86
MitchellGerdisch <mitch@pulumi.com>
2023-06-15 11:06:02 -0500

    pushing anyway

--- __main__.py
+++ __main__.py
@@ -1,3 +1,8 @@
 # App project Template
 import pulumi
 from pulumi_aws import s3
+
+my_bucket = s3.BucketV2('my-bucket')
+
+my_bucket_access_block = s3.BucketPublicAccessBlock('my-bucket-access-block',
+                                                    bucket=my_bucket.id)

d5523194ece0de69be52f7c85479a6d208439d9c
MitchellGerdisch <mitch@pulumi.com>
2023-06-15 11:14:10 -0500

    another push

--- __main__.py
+++ __main__.py
@@ -5,4 +5,6 @@ from pulumi_aws import s3
 my_bucket = s3.BucketV2('my-bucket')
 
 my_bucket_access_block = s3.BucketPublicAccessBlock('my-bucket-access-block',
-                                                    bucket=my_bucket.id)
+                                                    bucket=my_bucket.id, 
+                                                    # block_public_acls=True
+                                                    )

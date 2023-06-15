7cf91f491e62a1e033cc0c6f896f3e179b513e27
MitchellGerdisch <mitch@pulumi.com>
2023-06-15 16:19:46 -0500

    code is ready

--- __main__.py
+++ __main__.py
@@ -1,3 +1,9 @@
 # App project Template
 import pulumi
 from pulumi_aws import s3
+
+my_bucket = s3.BucketV2("my-bucket")
+
+# my_bucket = s3.BucketPublicAccessBlock("my-bucket-access-block",
+#                                        bucket = my_bucket.id,
+#                                        block_public_acls=True)
